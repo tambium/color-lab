fc236b10856c16d48b65f4f5535aff021d15dee5
Dominic Chapman <dominicchapman@me.com>
2020-02-13 16:49:27 +0000

    Use HSLuv

--- src/components/lessons/index.js
+++ src/components/lessons/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { convert } from "chromatism";
+import { hsluvToRgb } from "hsluv";
 
 const RGB_SET = [
   "#8C98A8",
@@ -14,26 +15,37 @@ const RGB_SET = [
 ];
 
 export const Lessons = ({ maxWidth }) => {
-  let CIELAB = [];
-  for (var j = -16; j <= 16; j++) {
-    CIELAB[CIELAB.length] = j * 8;
-  }
+  const [ls, setLS] = React.useState(50);
+
   return (
     <div
       style={{ maxWidth, marginLeft: "auto", marginRight: "auto", padding: 24 }}
     >
       <div style={{ marginBottom: 24 }}>
-        <span style={{ display: "block", fontWeight: 500, marginBottom: 8 }}>
+        <span
+          style={{
+            display: "block",
+            fontWeight: 500,
+            marginBottom: 8
+          }}
+        >
           Lessons
         </span>
-        <span style={{ fontSize: 14, fontWeight: 500 }}>RGB</span>
+        <span
+          style={{
+            fontSize: 14,
+            fontWeight: 500
+          }}
+        >
+          RGB
+        </span>
       </div>
       <div
         style={{
           backgroundColor: "#F8FAFC",
           display: "inline-flex",
           padding: 48,
-          marginBottom: 24
+          marginBottom: 48
         }}
       >
         {RGB_SET.map((color, idx) => {
@@ -69,8 +81,48 @@ export const Lessons = ({ maxWidth }) => {
           );
         })}
       </div>
+
       <div style={{ marginBottom: 24 }}>
-        <span style={{ fontSize: 14, fontWeight: 500 }}>HSL</span>
+        <span
+          style={{
+            display: "block",
+            fontSize: 14,
+            fontWeight: 500,
+            marginBottom: 8
+          }}
+        >
+          Lightness and saturation
+        </span>
+        <div style={{ alignItems: "center", display: "flex" }}>
+          <input
+            max="100"
+            min="0"
+            onChange={e => setLS(Number(e.target.value))}
+            step="1"
+            style={{ marginRight: 8 }}
+            type="range"
+            value={ls}
+          />
+          <span>{ls}</span>
+        </div>
+      </div>
+
+      <div style={{ marginBottom: 24 }}>
+        <span
+          style={{
+            display: "block",
+            fontSize: 14,
+            fontWeight: 500,
+            marginBottom: 8
+          }}
+        >
+          HSL
+        </span>
+        <span style={{ color: "#8992A1", fontSize: 14 }}>
+          “Notice that some of these colors appear lighter or more saturated
+          than others. For example, the blues appear especially dark and the
+          yellows and greens appear especially light.”
+        </span>
       </div>
       <div
         style={{
@@ -81,7 +133,7 @@ export const Lessons = ({ maxWidth }) => {
         }}
       >
         {new Array(360).fill(null).map((el, idx) => {
-          const hsl = `hsl(${idx}, 50%, 50%)`;
+          const hsl = `hsl(${idx}, ${ls}%, ${ls}%)`;
           return (
             <div
               key={idx}
@@ -95,8 +147,20 @@ export const Lessons = ({ maxWidth }) => {
         })}
       </div>
       <div style={{ marginBottom: 24 }}>
-        <span style={{ fontSize: 14, fontWeight: 500 }}>
+        <span
+          style={{
+            display: "block",
+            fontSize: 14,
+            fontWeight: 500,
+            marginBottom: 8
+          }}
+        >
           Perceptually uniform
+        </span>
+        <span style={{ color: "#8992A1", fontSize: 14 }}>
+          “These colors appear to blend together, and each color appears to be
+          just as light and as saturated as the rest. This is perceptual
+          uniformity at work.”
         </span>
       </div>
       <div
@@ -107,30 +171,17 @@ export const Lessons = ({ maxWidth }) => {
           marginBottom: 24
         }}
       >
-        {/* {new Array(256 / 8).fill(null).map((el, idx) => {
-          const ab = CIELAB[idx];
-          const cielch = { L: 100, C: 128, h: ab };
-          console.log(cielch);
-
-          return (
-            <div
-              key={idx}
-              style={{
-                backgroundColor: convert(cielch).hex,
-                height: 60,
-                width: 20
-              }}
-            />
-          );
-        })} */}
         {new Array(360).fill(null).map((el, idx) => {
-          const hsluv = { hu: idx, s: 60, l: 65 };
+          const colorHSLuv = (h, s, l) => {
+            const rgb = hsluvToRgb([h, s, l]);
+            return `rgb(${rgb[0] * 255}, ${rgb[1] * 255}, ${rgb[2] * 255})`;
+          };
 
           return (
             <div
               key={idx}
               style={{
-                backgroundColor: convert(hsluv).hex,
+                backgroundColor: colorHSLuv(idx, ls, ls),
                 height: 60,
                 width: 2
               }}
