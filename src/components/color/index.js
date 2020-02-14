3d97a058d19c1357feecb0216072e33c06319337
Dominic Chapman <dominicchapman@me.com>
2020-02-14 23:53:24 +0000

    Add sample chart

--- src/components/color/index.js
+++ src/components/color/index.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { hexToHsluv } from "hsluv";
+import { CartesianGrid, Line, LineChart } from "recharts";
 import { BORDER_WIDTH, PADDING, SHADE_WIDTH } from "../../constants";
 
 export const Color = ({ palette, palettePosition }) => {
   const shadeSet = palette[palettePosition[1]];
   const { title } = shadeSet;
 
+  let lightness = [];
+  shadeSet.shades.map(shade => {
+    lightness.push({
+      shade: shade.shade,
+      lightness: hexToHsluv(shade.hex)[2]
+    });
+  });
+
   return (
     <React.Fragment>
       <div style={{ marginBottom: 16 }}>
         <span style={{ fontSize: 21 }}>{title}</span>
       </div>
-      <table style={{ borderSpacing: 0 }}>
+      <table style={{ borderSpacing: 0, marginBottom: 32 }}>
         <thead>
           <tr>
             {shadeSet.shades.map(shade => (
@@ -62,6 +72,64 @@ export const Color = ({ palette, palettePosition }) => {
           </tr>
         </tbody>
       </table>
+      <div style={{ marginBottom: 16 }}>
+        <span style={{ fontWeight: 500 }}>Lightness</span>
+      </div>
+      <div
+        style={{
+          backgroundColor: "#F9FAFC",
+          border: "1px solid #E7E7E9",
+          width: 540
+        }}
+      >
+        <table style={{ borderSpacing: 0, borderBottom: "1px solid #E7E7E9" }}>
+          <tbody>
+            <tr>
+              {lightness.map(shade => (
+                <td
+                  key={shade.shade}
+                  style={{
+                    minWidth: SHADE_WIDTH,
+                    paddingBottom: PADDING,
+                    paddingTop: PADDING,
+                    textAlign: "center"
+                  }}
+                >
+                  <span
+                    style={{
+                      color: "#8992A1",
+                      fontSize: 12,
+                      fontWeight: 500
+                    }}
+                  >
+                    {shade.lightness.toFixed(1)}
+                  </span>
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
+
+        <LineChart
+          background="red"
+          data={lightness}
+          width={540}
+          height={175}
+          margin={{
+            right: 27,
+            left: 27
+          }}
+        >
+          <CartesianGrid stroke="none" />
+          <Line
+            isAnimationActive={false}
+            type="monotone"
+            dataKey="lightness"
+            stroke="#8884d8"
+            strokeWidth={2}
+          />
+        </LineChart>
+      </div>
     </React.Fragment>
   );
 };
